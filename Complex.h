2aea50e70103ad8a2920735e0d73b63dda775a53
samueltenka <iguavadon@gmail.com>
2014-08-06 13:33:46 -0400

    Create Complex.h
    Outlines basic complex number functionality, including *, +, and ^ (powering).

---
+++ Complex.h
@@ -0,0 +1,45 @@
+#include <math.h>
+#include <cstdio>
+
+
+class Complex {
+private:
+	double real, imag;
+public:
+	//
+	// Constructors:
+	Complex():
+		real(0), imag(0) {}
+	Complex(double real, double imag): // cartesian
+		real(real), imag(imag) {}
+	Complex(double angle, double length, bool conjugate): // polar
+		real(length * cos(angle)),
+		imag(length * sin(angle) * (conjugate? -1 : 1)) {}
+
+	// Assignment:
+	Complex operator=(double other) {
+		real = other, imag = 0;
+		return (*this);
+	}
+	Complex operator=(const Complex& other) {
+		real = other.real, imag = other.imag;
+		return (*this);
+	}
+
+	// Basic Arithmetic:
+	Complex operator*(double other) const;
+	Complex operator*(const Complex& other) const;
+	Complex operator+(const Complex& other) const;
+
+	// Exponentiation (by means of polar coordinates):
+	Complex operator^(const Complex& power) const;
+	double angle() const;
+	double length() const;
+	Complex logarithm() const;				
+	Complex exponential() const;
+
+	// Printing:
+	void print() {
+		printf("%f + %fi\n", real, imag);
+	}
+};
