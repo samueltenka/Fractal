b54461840669c3e41d379a9f043bb2c858950f2e
samueltenka <iguavadon@gmail.com>
2014-08-06 13:53:22 -0400

    Update Main.cpp
    Wrote "escape_time"-measurer that takes in mandelbrot-type formula (f: \C^2 \to \C),
    as well as simple ASCII Art Renderer.
    
    IT WORKS! OUTPUT:
    
                                  #
                                  #
                                  #
                                  #
                                 ###
                                  #
                               #######
                             ###########
                              #########
                             ###########
                             ###########
                              #########
                              #########
                              #########
                               #######
                                  #
                             ###########
                           ###############
                          #################
                       # ################### #
                      #########################
                       #######################
                       #######################
                      #########################
                      #########################
                      #########################
                  ### ######################### ###
                  ### ######################### ###
                 #### ######################### ####
                  ### ######################### ###
              #       #########################       #
                      #########################
                      #########################
                       #######################
                       #######################
                       #######################
                       ## ######## ######## ##
                           ######   ######
                           #             #

--- Main.cpp
+++ Main.cpp
@@ -1,28 +1,45 @@
 #include "Complex.h"
 
 
-//Complex mandelbrot(Complex z, Complex c)
-//{
-//	return z*z + c;
-//}
-//
-//double value(Complex c, Complex (*map)(Complex, Complex))
-//{
-//	Complex z = c;
-//	z = map(z, c);
-//}
+Complex mandelbrot(Complex z, Complex c) {
+	return z*z + c;
+}
 
+int escape_time(Complex c, Complex (*map)(Complex, Complex), Complex initial, double threshold, int max_steps) {
+	Complex z = initial;
+
+	for(int steps = 0; steps < max_steps; steps++) {
+		if(z.length() > threshold) {
+			return steps;
+		}
+		z = map(z, c);
+	}
+	return -1;
+}
+int escape_time(Complex c, Complex (*map)(Complex, Complex)) {
+	return escape_time(c, map, Complex(0, 0), 100.0, 256);
+}
+
+int color(int num_steps) {
+	if(num_steps >= 0) {
+		return num_steps > 255 ? 255 : num_steps;
+	}
+	return 0; // got -1, meaning never escaped. => black.
+}
 
 #include <iostream>
 using namespace std;
 void main() {
-	Complex C(2, 1);
-	Complex Z = C;
-	for(int i = 0; i < 5; i++) {
-		Z.print();
-		Z = Z*C;
+	// Test Mandelbrot escape calculations:
+	for(int r = 0; r < 60; r++) {
+		for(int c = 0; c < 60; c++) {
+			Complex C(r/20.0 - 1.5, c/20.0 - 1.5);
+			int e = escape_time(C, &mandelbrot);
+			cout << (e==-1? '#' : ' ');
+		}
+		cout << endl;
 	}
-	
-	char l;
-	cin >> l;
+
+	// Pause before exiting:
+	cout << "tada!" << endl; char l; cin >> l;
 }
