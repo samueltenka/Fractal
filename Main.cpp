9f6d11e98c7519a69f70ae8b5695f9250cc5a38e
samueltenka <iguavadon@gmail.com>
2014-08-10 00:05:00 -0400

    Update Main.cpp
    Made output image dimensions and rectangle in C adjustable (and hence not necessarily to scale).
    e.g.
    
    http://1.bp.blogspot.com/-7KymRNrLdj4/U-bthk3FeWI/AAAAAAAABJs/lnghesjdbic/s1600/Mz_DB.png

--- Main.cpp
+++ Main.cpp
@@ -3,33 +3,51 @@
 using namespace std;
 #include "GraphicsBitmap.h"
 
+/*
+#define PIXEL_SIDE_H (1920*5)
+#define PIXEL_SIDE_W (1080*5)
 
-// CRACK LOCATION:
-// (2 * -(256.0-160.0)/256.0, 0)
+const double LEFT = -2.7;
+const double RIGHT = 0.8;
+const double LOWER = -3.11111111111;
+const double UPPER = 3.11111111111;
 
-// TAIL LOCATION:
-// (2 * -(256.0-31.0)/256.0, 0)
+const double HEIGHT = (UPPER-LOWER);
+const double WIDTH = (RIGHT-LEFT);
+const double CENTER_R = (LOWER + HEIGHT/2);
+const double CENTER_I = (LEFT + WIDTH/2);
+*/
 
-#define PIXEL_SIDE 256
-#define CMPLX_SIDE 3.0//4.0
-#define CENTER_R -1.409
-#define CENTER_I 0.0
+#define PIXEL_SIDE_H 256
+#define PIXEL_SIDE_W 256
+
+const double LEFT = -2;
+const double RIGHT = +2;
+const double LOWER = -2;
+const double UPPER = 2;
+
+const double HEIGHT = (UPPER-LOWER);
+const double WIDTH = (RIGHT-LEFT);
+const double CENTER_R = (LOWER + HEIGHT/2);
+const double CENTER_I = (LEFT + WIDTH/2);
+
+
 struct coor {
 	int r, c;
 	bool is_in_bounds() {
-		return (0 <= r && r < PIXEL_SIDE) &&
-				(0 <= c && c < PIXEL_SIDE);
+		return (0 <= r && r < PIXEL_SIDE_H) &&
+				(0 <= c && c < PIXEL_SIDE_W);
 	}
 };
-coor to_pixel(Complex Z) {
-	coor rtrn;
-	rtrn.r = static_cast<int>((Z.imag - (CENTER_I-CMPLX_SIDE/2)) * (PIXEL_SIDE/CMPLX_SIDE));
-	rtrn.c = static_cast<int>((Z.real - (CENTER_R-CMPLX_SIDE/2)) * (PIXEL_SIDE/CMPLX_SIDE));
-	return rtrn;
-}
+//coor to_pixel(Complex Z) {
+//	coor rtrn;
+//	rtrn.r = static_cast<int>((Z.imag - LOWER) * (PIXEL_SIDE_H/HEIGHT));
+//	rtrn.c = static_cast<int>((Z.real - LEFT) * (PIXEL_SIDE_W/WIDTH));
+//	return rtrn;
+//}
 Complex to_cmplx(int r, int c) {
-	return Complex(CENTER_R + c*(CMPLX_SIDE/PIXEL_SIDE) - CMPLX_SIDE/2,   // centered on origin
-					CENTER_I + r*(CMPLX_SIDE/PIXEL_SIDE) - CMPLX_SIDE/2); //
+	return Complex(LEFT + c*(WIDTH/PIXEL_SIDE_W),
+					LOWER + r*(HEIGHT/PIXEL_SIDE_H));
 }
 
 void main() {
@@ -58,11 +76,11 @@ void main() {
 	//}
 	//I.write_to("C:\\Users\\Sam\\Documents\\Visual Studio 2012\\Projects\\Fractal\\Fractal\\JL.bmp");
 
-	Bitmap I(PIXEL_SIDE, PIXEL_SIDE);
-	for(int r = 0; r < PIXEL_SIDE; r++) {
-		for(int c = 0; c < PIXEL_SIDE; c++) {
+	Bitmap I(PIXEL_SIDE_H, PIXEL_SIDE_W);
+	for(int r = 0; r < PIXEL_SIDE_H; r++) {
+		for(int c = 0; c < PIXEL_SIDE_W; c++) {
 			Complex C = to_cmplx(r, c);
-			RGB X = limit_color(C, &alech);
+			RGB X = limit_color(C, &mandelbrot);
 			I.paint_pixel(r, c, X);
 		}
 		if(r % 16 == 0) cout << '.';
